6ece79a958872f47809b3ee4f7dae3102e21855e
Bert Outtier <outtierbert@gmail.com>
2026-05-08 09:52:55 +0200

    add usb-device-midi from microptyhon-lib to the image

--- manifests/manifest.py
+++ manifests/manifest.py
@@ -1,3 +1,5 @@
 freeze('../internal_filesystem/', 'main.py') # Hardware initialization
 freeze('../internal_filesystem/lib', '') # Additional libraries
 freeze('../freezeFS/', 'freezefs_mount_builtin.py') # Built-in apps
+package("usb", base_path="../lvgl_micropython/lib/micropython/lib/micropython-lib/micropython/usb/usb-device")
+package("usb", base_path="../lvgl_micropython/lib/micropython/lib/micropython-lib/micropython/usb/usb-device-midi")
