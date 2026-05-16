d0174793e352bc0b47f2f5c40d47291f4653e194
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-05-16 18:31:36 +0200

    Release 0.9.6

--- internal_filesystem/lib/mpos/build_info.py
+++ internal_filesystem/lib/mpos/build_info.py
@@ -9,5 +9,5 @@ class BuildInfo:
     class version:
         """Version information."""
 
-        release = "0.9.5"
+        release = "0.9.6"
         api_level = 0  # subject to change until API Level 1
